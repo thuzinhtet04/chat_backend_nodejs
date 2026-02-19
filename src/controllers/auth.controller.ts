6a3aa2007376391094e26d1893e28a0b8405d5d6
thuzinhtet <thuzinhtet.dev@gmail.com>
2026-02-19 17:16:57 +0630

    feat/loginController

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -37,3 +37,28 @@ export const signup = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Internal Server Errror" });
   }
 };
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+    generateToken(user._id.toString(), res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Login Controller", (error as Error).message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const logout = (req: Request, res: Response) => {
+  res.send("logout route");
+};
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,9 @@
 import { type Request, Router } from "express";
-import { signup } from "../controllers/auth.controller";
+import { login, signup } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/sign-up", signup);
-router.post("/login", (req: Request, res) => {
-  const authHeader = req.headers.authorization;
-  console.log(authHeader);
-  res.status(401).json({ message: "no auth" });
-});
+router.post("/sign-in", login);
 router.post("/logout", () => {});
 export default router;
